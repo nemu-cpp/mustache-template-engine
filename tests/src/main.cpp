917e02836db8759b3daacbe56fc9a851312b76b3
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-11 17:14:26 +0000

    Updates

---
+++ tests/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/nemu-cpp/mustache-templating-engine/blob/main/LICENSE.txt
+*/
+
+#include <Ishiko/Tests.hpp>
+
+using namespace Ishiko::Tests;
+
+int main(int argc, char* argv[])
+{
+    TestHarness theTestHarness("NemuMustacheTemplatingEngine");
+
+    return theTestHarness.run();
+}
