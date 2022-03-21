a2053d8d0c6c805671b316f44aa1271b1fd93df8
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-21 22:28:57 +0000

    Rename files

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -5,7 +5,7 @@
 */
 
 #include "MustacheTemplatingEngineTests.hpp"
-#include "Nemu/MustacheTemplatingEngine/linkoptions.hpp"
+#include "Nemu/MustacheTemplateEngine/linkoptions.hpp"
 #include <Ishiko/Tests.hpp>
 
 using namespace Ishiko::Tests;
