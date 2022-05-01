26c417415926f5c252cab0bb5a34e2dc8667d098
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-01 15:18:07 +0100

    Add tests (#28)

--- tests/src/MustacheTemplateEngineTests.cpp
+++ tests/src/MustacheTemplateEngineTests.cpp
@@ -16,6 +16,7 @@ MustacheTemplateEngineTests::MustacheTemplateEngineTests(const TestNumber& numbe
 {
     append<HeapAllocationErrorsTest>("Creation test 1", ConstructorTest1);
     append<HeapAllocationErrorsTest>("render test 1", RenderTest1);
+    append<HeapAllocationErrorsTest>("render test 2", RenderTest2);
 }
 
 void MustacheTemplateEngineTests::ConstructorTest1(Test& test)
@@ -31,15 +32,37 @@ void MustacheTemplateEngineTests::RenderTest1(Test& test)
     MustacheTemplateEngine templatingEngine(MustacheTemplateEngine::Options(templateRootDir.string()));
 
     ViewContext context;
-    std::string renderedView = templatingEngine.render("index.html", context);
+    std::string renderedView = templatingEngine.render("MustacheTemplateEngineTests_RenderTest1.html", context);
 
     boost::filesystem::path outputPath =
         test.context().getTestOutputPath("MustacheTemplateEngineTests_RenderTest1.html");
     Error error; // TODO: use exception
     BinaryFile outputFile = BinaryFile::Create(outputPath, error);
     outputFile.write(renderedView.c_str(), renderedView.size());
+    outputFile.close();
 
     ISHIKO_TEST_FAIL_IF_FILES_NEQ("MustacheTemplateEngineTests_RenderTest1.html",
         "MustacheTemplateEngineTests_RenderTest1.html");
+    ISHIKO_TEST_PASS();
+}
+
+void MustacheTemplateEngineTests::RenderTest2(Test& test)
+{
+    boost::filesystem::path templateRootDir = test.context().getTestDataPath("templates");
+    MustacheTemplateEngine templatingEngine(MustacheTemplateEngine::Options(templateRootDir.string()));
+
+    ViewContext context;
+    context["name"] = "John";
+    std::string renderedView = templatingEngine.render("MustacheTemplateEngineTests_RenderTest2.html", context);
+
+    boost::filesystem::path outputPath =
+        test.context().getTestOutputPath("MustacheTemplateEngineTests_RenderTest2.html");
+    Error error; // TODO: use exception
+    BinaryFile outputFile = BinaryFile::Create(outputPath, error);
+    outputFile.write(renderedView.c_str(), renderedView.size());
+    outputFile.close();
+
+    ISHIKO_TEST_FAIL_IF_FILES_NEQ("MustacheTemplateEngineTests_RenderTest2.html",
+        "MustacheTemplateEngineTests_RenderTest2.html");
     ISHIKO_TEST_PASS();
 }
