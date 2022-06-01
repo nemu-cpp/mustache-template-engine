be647e53ff6581228c911833601bc9e84279b447
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-06-01 20:10:02 +0100

    Add files

---
+++ include/Nemu/MustacheTemplateEngine/MustacheTemplateEngineProfile.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/nemu-cpp/mustache-template-engine/blob/main/LICENSE.txt
+*/
+
+#ifndef _NEMU_CPP_MUSTACHETEMPLATEENGINE_MUSTACHETEMPLATEENGINEPROFILE_HPP_
+#define _NEMU_CPP_MUSTACHETEMPLATEENGINE_MUSTACHETEMPLATEENGINEPROFILE_HPP_
+
+#endif
---
+++ src/MustacheTemplateEngineProfile.cpp
@@ -0,0 +1,7 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/nemu-cpp/mustache-template-engine/blob/main/LICENSE.txt
+*/
+
+#include "MustacheTemplateEngineProfile.hpp"
